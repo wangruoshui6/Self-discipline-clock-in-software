39fad7b15f3053a401fcc1a6979591b61572802c
wangruoshui6 <16633445526@163.com>
2025-08-08 22:14:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/Self-discipline-clock-in-software/', // 修正为你的 GitHub 仓库名称
   plugins: [vue()],
   server: {
     port: 3000,
     open: true
   }
-}) 
+})
